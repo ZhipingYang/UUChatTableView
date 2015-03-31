ebb79a5e8a7b8b8f0d2f3b88b71af1f6bce45c41
丁南 <340052204@qq.com>
2015-03-31 12:20:39 +0800

    added applicationWillResignActive delegate to UUAVAudioPlayer.m, so when app state is from inactive to active, the audio will not continue.(I think from user experience, when app is back to active, there should be no voice to "shock" users.)

--- Demo/UUChat/UUAVAudioPlayer.m
+++ Demo/UUChat/UUAVAudioPlayer.m
@@ -28,55 +28,47 @@
 
 -(void)playSongWithUrl:(NSString *)songUrl
 {
-    dispatch_async(dispatch_queue_create("dfsfe", NULL), ^{
-        
+    
+    dispatch_async(dispatch_queue_create("playSoundFromUrl", NULL), ^{
         [self.delegate UUAVAudioPlayerBeiginLoadVoice];
-        
         NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:songUrl]];
         
         dispatch_async(dispatch_get_main_queue(), ^{
-            if (_player) {
-                [self.delegate UUAVAudioPlayerDidFinishPlay];
-                [_player stop];
-                _player.delegate = nil;
-                _player = nil;
-            }
-            [[NSNotificationCenter defaultCenter]postNotificationName:@"VoicePlayHasInterrupt" object:nil];
-            NSError *playerError;
-            _player = [[AVAudioPlayer alloc]initWithData:data error:&playerError];
-            _player.volume = 1.0f;
-            if (_player == nil){
-                NSLog(@"ERror creating player: %@", [playerError description]);
-            }
-            [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayback error: nil];
-            _player.delegate = self;
-            [_player play];
-            [self.delegate UUAVAudioPlayerBeiginPlay];
+            [self playSoundWithData:data];
         });
     });
 }
 
 -(void)playSongWithData:(NSData *)songData
 {
-    [self.delegate UUAVAudioPlayerDidFinishPlay];
+    [self setupPlaySound];
+    [self playSoundWithData:songData];
+}
 
+-(void)playSoundWithData:(NSData *)soundData{
     if (_player) {
+        [self.delegate UUAVAudioPlayerDidFinishPlay];
         [_player stop];
         _player.delegate = nil;
         _player = nil;
     }
     [[NSNotificationCenter defaultCenter]postNotificationName:@"VoicePlayHasInterrupt" object:nil];
     NSError *playerError;
-    _player = [[AVAudioPlayer alloc]initWithData:songData error:&playerError];
+    _player = [[AVAudioPlayer alloc]initWithData:soundData error:&playerError];
     _player.volume = 1.0f;
-    [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayback error: nil];
+    if (_player == nil){
+        NSLog(@"ERror creating player: %@", [playerError description]);
+    }
     _player.delegate = self;
     [_player play];
     [self.delegate UUAVAudioPlayerBeiginPlay];
-
 }
 
-
+-(void)setupPlaySound{
+    UIApplication *app = [UIApplication sharedApplication];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationWillResignActive:) name:UIApplicationWillResignActiveNotification object:app];
+    [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayback error: nil];
+}
 
 - (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag
 {
@@ -88,6 +80,10 @@
     if (_player && _player.isPlaying) {
         [_player stop];
     }
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application{
+    [self.delegate UUAVAudioPlayerDidFinishPlay];
 }
 
 @end
