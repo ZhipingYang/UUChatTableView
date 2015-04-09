2e436e7340d31b9199ae4b239146022cfa483bfb
丁南 <340052204@qq.com>
2015-04-09 23:28:07 +0800

    only when the voice is playing, the proximity monitor turns on

--- Demo/UUChatTableView/RootViewController.m
+++ Demo/UUChatTableView/RootViewController.m
@@ -31,9 +31,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //开启红外线感应
-    [[UIDevice currentDevice] setProximityMonitoringEnabled:YES];
-
     [self initBar];
     [self addRefreshViews];
     [self loadBaseViewsAndData];
